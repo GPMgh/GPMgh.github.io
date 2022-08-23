d2fbf0a8276fe6022eb32907099f06dc132c1738
GPMgh <111754668+GPMgh@users.noreply.github.com>
2022-08-23 20:17:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+//Extenion function
+ String.removeWhitespaces() = replace(" ", "")
+
+// Uses
+var str = "This is an example text".removeWhitespaces()
+
+println(str)
+
+
+
+
+
+
+
+
+
+
